2a333eb19669dc1edab571de7c11af69bb27c645
saniag081 <sgonzalezhernandez081@gmail.com>
2018-10-19 16:35:24 -0500

    responsive

---
+++ js/index.js
@@ -0,0 +1,31 @@
+
+
+var $bugermenu = document.getElementById("burger-buttom");
+var $menu = document.getElementById("menu");
+
+var consulta = window.matchMedia('(max-width:640px)');
+consulta.addListener(mediaQuery);
+
+function mediaQuery() {
+    if (consulta.matches) {
+        console.log("se cumplio");
+
+        $bugermenu.addEventListener('click', togleMenu);
+    }
+    else{
+        $bugermenu.removeEventListener('click', togleMenu);
+        console.log("no se cumplio");
+    }
+}
+
+mediaQuery();
+
+function togleMenu() {
+    $menu.classList.toggle('active')   
+}
+function showMenu() {
+        $menu.classList.add('active')
+}
+function hideMenu() {
+    $menu.classList.remove('active')
+}
