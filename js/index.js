8393bebbd332a5d725ffe56f9ee2c583a5dae928
saniag081 <sgonzalezhernandez081@gmail.com>
2018-11-03 20:40:17 -0500

    animation menu a

--- js/index.js
+++ js/index.js
@@ -1,4 +1,25 @@
+$(function () {
 
+    $('a[href*=#]').click(function () {
+
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            && location.hostname == this.hostname) {
+
+            var $target = $(this.hash);
+
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+
+                $('html,body').animate({ scrollTop: targetOffset }, 1000);
+
+                return false;
+            }
+        }
+    });
+
+});
 
 var $bugermenu = document.getElementById("burger-buttom");
 var $menu = document.getElementById("menu");
@@ -23,9 +44,9 @@ mediaQuery();
 function togleMenu() {
     $menu.classList.toggle('active')   
 }
-function showMenu() {
-        $menu.classList.add('active')
-}
-function hideMenu() {
-    $menu.classList.remove('active')
-}
+// function showMenu() {
+//         $menu.classList.add('active')
+// }
+// function hideMenu() {
+//     $menu.classList.remove('active')
+// }
